a29e773f82461fd0d4c5a92e720a1db407f0f9e8
vvctort1 <rm551649@fiap.com.br>
2023-09-18 09:56:04 -0300

    listagem dinâmica realizada

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,21 @@
+export const ListaProduto=[
+    {
+        id:1,
+        nome:"Teclado",
+        desc:"Teclado Mecânico 102 Teclas.",
+        preco:220
+    },
+    {
+        id:2,
+        nome:"Mouse",
+        desc:"Mouse sem fio.",
+        preco:55.50
+    },
+    {
+        id:1,
+        nome:"Monitor",
+        desc:"Monitor de 32' 75Hz.",
+        preco:400
+    }
+
+];
--- src/routes/Produtos/index.jsx
+++ src/routes/Produtos/index.jsx
@@ -1,8 +1,42 @@
+import { ListaProduto } from "../../components/ListaProdutos"
+
+
 export default function Produtos(){
+
+    document.title = "Lista de Produtos";
 
     return(
         <main>
             <h1>Produtos</h1>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NOME</th>
+                        <th>DESCRIÇÃO</th>
+                        <th>PREÇO</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ListaProduto.map((item, indice)=>
+                        <tr key={indice}>
+                            <td>{item.id}</td>
+                            <td>{item.nome}</td>
+                            <td>{item.desc}</td>
+                            <td>{item.preco}</td>
+                        </tr>
+                )}
+
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={4}>PRODUTOS INFORMÁTICOS - QTD = {ListaProduto.length}</td>
+                    </tr>
+                </tfoot>
+
+
+            </table>
         </main>
     )
 }
