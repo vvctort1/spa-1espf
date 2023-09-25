6adc5b90635fe2d5dca7aeb2c6ef8624e9fb6f75
vvctort1 <rm551649@fiap.com.br>
2023-09-25 09:15:46 -0300

    imagem public

--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 
+
 export default function EditarProduto(){
 
     const {id} = useParams();
@@ -12,6 +13,7 @@ export default function EditarProduto(){
         <main>
             <h1>Editando o produto</h1>
             <h2>Produtos Selecionados : {id}</h2>
+            <img src="/img/vite.svg" alt="" />
         </main>
     )
 }
