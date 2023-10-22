ec14d5ab23f1081c2552cc0864257fdcf5709d9f
MuriloEloys <rm552117@fiap.com.br>
2023-10-22 20:40:24 -0300

    Finalização do código

--- src/routes/Error404/index.jsx
+++ src/routes/Error404/index.jsx
@@ -2,7 +2,7 @@ export default function Error(){
 
     return(
         <>
-            <h1>Erro 404 - Página não encontrada</h1>
+            <h1>Erro 404 - Página não encontrada!</h1>
         </>
     )
 }
