938dc654f576d10a10c462f9c4a1c9b2751e4e29
vvctort1 <rm551649@fiap.com.br>
2023-10-30 09:56:15 -0300

    validação do formulario

--- src/routes/Login/Login.jsx
+++ src/routes/Login/Login.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react"
+import { useNavigate } from "react-router-dom";
 
 
 export default function Login() {
+
+    //CRIANDO O REDIRECIONADOR
+    const navigate = useNavigate();
 
     const [usuario,setUsuario] = useState({
         email: "",
         senha: ""
     })
-
-    
-
 
     //PREENCHIMENTO DO FORM
     const handleChange = (e) => {
@@ -17,7 +18,37 @@ export default function Login() {
       const {name,value} = e.target;
       //PREENCHENDO O USE-STATE COM OS VALORES DA DESESTRUTURAÇÃO, UTILIZANDO O OPERADOR SPREAD.
       setUsuario({...usuario,[name]:value})
-      
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    let users;
+
+    try {
+        const response = await fetch("http://localhost:5000/usuarios");
+        users = await response.json();
+    } catch (error) {
+        alert("Ocorreu um erro no processamento da sua solicitação!");
+    }
+
+    //REALIZANDO A VALIDAÇÃO DO USUÁRIO.
+    for (let x = 0; x < users.length; x++){
+        const user = users[x];
+        //REALIZANDO A COMPARAÇÃO DE FATO!
+        if (user.email == usuario.email && user.senha == usuario.senha){
+            alert("Login realizado com sucesso!")
+            navigate('/',{replace: true})
+
+            return;
+        }
+    }
+
+    alert("Login ou senha incorretos")
+    setUsuario({
+        email: "",
+        senha: ""
+    });
   }
 
   return (
@@ -25,7 +56,7 @@ export default function Login() {
         <h1>Login</h1>
 
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <fieldset>
                     <legend>User Information:</legend>
                     <div>
